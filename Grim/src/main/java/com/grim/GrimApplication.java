09faf01a2c4c881f4622b5bd6d583aa018c4bc67
JiJohnYuno <gng0202@naver.com>
2025-02-10 15:58:32 +0900

    commit
    commit

--- Grim/src/main/java/com/grim/GrimApplication.java
+++ Grim/src/main/java/com/grim/GrimApplication.java
@@ -3,11 +3,11 @@ package com.grim;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class GrimApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrimApplication.class, args);
 	}
 
-}
+}  
