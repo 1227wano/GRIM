d416f13cb5b88912601fd4d29a14a973b5215618
1227wano <1227wanho@gmail.com>
2025-02-12 22:24:10 +0900

    250212 wano commit

--- Grim/src/main/java/com/grim/configuration/security/SecurityConfigure.java
+++ Grim/src/main/java/com/grim/configuration/security/SecurityConfigure.java
@@ -53,7 +53,7 @@ public class SecurityConfigure {
 				.csrf(AbstractHttpConfigurer::disable) // csrf 비활성화
 				.cors(Customizer.withDefaults()) // 얘는 일단 꺼놓고 나중에 nginx붙이기
 				.authorizeHttpRequests(requests -> {
-					requests.requestMatchers("/members", "/members/login", "/uploads/**","/members/signup").permitAll(); // 인증없이 이용할 수 있음
+					requests.requestMatchers("/members", "/members/login", "/uploads/**","/members/signup","/museum/apiMuseum","/museum/realMuseum").permitAll(); // 인증없이 이용할 수 있음
 					requests.requestMatchers(HttpMethod.PUT,"/members").authenticated(); // 인증해야 이용할 수 있음
 					requests.requestMatchers(HttpMethod.PUT,"/admin/**").hasRole("ADMIN");  //ADMIN 권한만 이용할 수 있음
 				})
--- Grim/src/main/java/com/grim/museum/model/mapper/MuseumMapper.java
+++ Grim/src/main/java/com/grim/museum/model/mapper/MuseumMapper.java
@@ -1,8 +1,14 @@
 package com.grim.museum.model.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+
+import com.grim.museum.model.dto.MuseumDTO;
 
 @Mapper
 public interface MuseumMapper {
+
+	@Insert("INSERT INTO TB_MUSEUM VALUES(#{museumName}, #{museumSidoname}, #{museumOpen}, #{museumClose})")
+	void saveMuseum(MuseumDTO museum);
 
 }
