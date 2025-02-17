87959c65a57e9ad55d8e7365a7adb7481eeb2077
JiJohnYuno <gng0202@naver.com>
2025-02-17 09:36:15 +0900

    commit
    commit

--- Grim/src/main/java/com/grim/paint/model/dto/PaintDTO.java
+++ Grim/src/main/java/com/grim/paint/model/dto/PaintDTO.java
@@ -21,12 +21,13 @@ public class PaintDTO {
 	
 	@NotBlank(message="❌ 제목은 비워둘 수 없습니다.")
 	@Size(min = 1, max = 30, message="❌ 제목은 1글자 이상 30글자 이하만 사용할 수 있습니다.")
+	@Pattern(regexp="^[0-9a-zA-Zㄱ-힣!@#$%^&*?\s]+$", message="❌ 내용에 한글, 영어, 특수문자는(!, @, #, $, %, ^, &, *, ?)\n   만 사용할 수 있습니다.")
 	private String picTitle;
 	
 	
 	@NotBlank(message="❌ 내용은 비워둘 수 없습니다.")
 	@Size(min = 1, max = 30, message="❌ 내용은 1글자 이상 600글자 이하만 사용할 수 있습니다.")
-	@Pattern(regexp="^[0-9a-zA-Z가-힣!@#$%^&*?\s]+$", message="❌ 내용에 한글, 영어, 특수문자는(!, @, #, $, %, ^, &, *, ?)\n   만 사용할 수 있습니다.")
+	@Pattern(regexp="^[0-9a-zA-Zㄱ-힣!@#$%^&*?\s]+$", message="❌ 내용에 한글, 영어, 특수문자는(!, @, #, $, %, ^, &, *, ?)\n   만 사용할 수 있습니다.")
 	private String picContent;
 	
 	private Date writeDate;
