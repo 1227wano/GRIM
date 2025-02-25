40610310d6975dc1bdb7e33e9364347680c29b1a
seohansol <snake1141@naver.com>
2025-02-25 19:02:23 +0900

    ㅅ

--- Grim/src/main/java/com/grim/auth/service/UserServiceImpl.java
+++ Grim/src/main/java/com/grim/auth/service/UserServiceImpl.java
@@ -31,7 +31,7 @@ public class UserServiceImpl implements UserDetailsService {
 		if(user == null) {
 			throw new UsernameNotFoundException("아이디 혹은 비밀번호가 일치하지 않습니다.");
 		}
-		// 사용자가 입력한 아이디값이 테이블에 존재하긴 함
+
 		return CustomUserDetails.builder()
 								.userNo(user.getUserNo())
 								.username(user.getUserId())
